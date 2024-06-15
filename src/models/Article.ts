aa9a01c4fe2a0401f45de99788639292eeb654d2
xder <vlasenko12ava@gmail.com>
2024-06-15 15:31:06 +0500

    another

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -18,44 +18,32 @@ sequelize
 		await Article.bulkCreate([
 			{
 				title: 'Статья defaultUser',
-				content: `# Dillinger
-				## _The Last Markdown Editor, Ever_
-				
-				[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
-				
-				[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
-				
-				Dillinger is a cloud-enabled, mobile-ready, offline-storage compatible,
-				AngularJS-powered HTML5 Markdown editor.
-				
-				- Type some Markdown on the left
-				- See HTML in the right
-				- ✨Magic ✨
-				
-				## Features
-				
-				- Import a HTML file and watch it magically convert to Markdown
-				- Drag and drop images (requires your Dropbox account be linked)
-				- Import and save files from GitHub, Dropbox, Google Drive and One Drive
-				- Drag and drop markdown and HTML files into Dillinger
-				- Export documents as Markdown, HTML and PDF
-				
-				Markdown is a lightweight markup language based on the formatting conventions
-				that people naturally use in email.
-				As [John Gruber] writes on the [Markdown site][df1]
-				
-				> The overriding design goal for Markdown's
-				> formatting syntax is to make it as readable
-				> as possible. The idea is that a
-				> Markdown-formatted document should be
-				> publishable as-is, as plain text, without
-				> looking like it's been marked up with tags
-				> or formatting instructions.
-				
-				This text you see here is *actually- written in Markdown! To get a feel
-				for Markdown's syntax, type some text into the left window and
-				watch the results in the right.`,
-				creatorId: 1, // Привязываем статью к defaulUser
+				content: `Настройка шлюзов ETL-процесса
+В случае распараллеливании схем ETL-процессов используются шлюзы. При этом настраиваемые элементы управляют порядком загрузки данных ETL.
+
+![Схема ETL шлюза](../assets/4.jpg)
+Схема ETL шлюза
+
+Для изменения типа шлюза используется пиктограмма ![Пиктограмма изменения типа шлюза](../assets/5.jpg).
+
+Типы шлюзов
+Эксклюзивный шлюз.
+Шлюз данного типа срабатывает при поступлении хотя бы одного потока данных. При нескольких потоках исходящих данных, выполнение схемы продолжается только по одному из них. Для этого шлюз в цикле проверяет все потоки и выбирает первый, для которого выполнится одно из условий:
+нет признака условного перехода;
+есть признак условного перехода, но не задан алгоритм для условия;
+алгоритм условного перехода вернул true.
+
+Если ни для одного из исходящих потоков не выполнилось ни одно условие, то выполнение продолжится по переходу по умолчанию.
+
+
+Инклюзивный (неэксклюзивный) шлюз.
+Элемент срабатывает каждый раз при получении потока данных. Данный шлюз может инициировать от одного до нескольких исходящих потоков данных. Определение перечня сработавших исходящих потоков данных выполняется синхронно (аналогично алгоритму эксклюзивного шлюза). Если в настройках задается количество потоков больше 1, то запускаются все сработавшие потоки в соответствии с их количеством. Если указан 1, все сработавшие потоки данных запустятся по порядку в синхронном режиме.
+
+Параллельный шлюз.
+Данный шлюз срабатывает только при выполнении всех входящих потоков. Далее обработка элемента и инициация исходящих потоков выполняется аналогично работе с инклюзивным шлюзом.
+
+Количество входящих потоков для инклюзивного (неэксклюзивный) и параллельного шлюзов указывается при настройке посредством пиктограммы ![Пиктограмма количества входящих потоков](../assets/6.jpg).`,
+				creatorId: 2, // Привязываем статью к defaulUser
 			},
 			{
 				title: 'Статья analyst',
