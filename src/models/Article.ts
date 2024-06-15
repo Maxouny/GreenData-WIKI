530806bcb1812e381e04b3ad4fd7df7716cbf9cf
Maxouny <sem159rus@gmail.com>
2024-06-15 13:49:16 +0500

    add art

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -11,8 +11,28 @@ class Article extends Model {
 }
 sequelize
 	.sync({ force: true }) // Установите force: true, чтобы сбросить существующие таблицы и создать их заново
-	.then(() => {
+	.then(async () => {
 		console.log('All models were synchronized successfully.')
+
+		// Создаем 3 пользователей после синхронизации моделей
+		await Article.bulkCreate([
+			{
+				title: 'Статья defaultUser',
+				content: 'Content for article 1',
+				creatorId: 1, // Привязываем статью к defaulUser
+			},
+			{
+				title: 'Статья analyst',
+				content: 'Content for article 2',
+				creatorId: 2, // Привязываем статью к analyst
+			},
+			{
+				title: 'Статья admin',
+				content: 'Content for article 3',
+				creatorId: 3, // Привязываем статью к admin
+			},
+		])
+		console.log('Users have been created successfully.')
 	})
 	.catch(err => {
 		console.error('An error occurred while synchronizing models:', err)
